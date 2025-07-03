923d5d6b32fd2826a00006ebe3bfa37ecd1daf0e
SpeOwO <thkim42750@gmail.com>
2025-07-03 22:43:36 +0900

    chore: clear main code

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ def run(board_url: str, save_path: str):
         download_image(url, save_path, file_name)
         sleep()
 
+# main
 if __name__ == '__main__':
-    # example
     board_url = "https://kr.pinterest.com/flat1000/wallpaper/"
     save_path = "d:/test"
-    download_image("https://i.pinimg.com/originals/dc/ae/57/dcae57655cff2f15584d325a80c1edbb.jpg", "c:/test", "test3.jpg")
-    #run(board_url, save_path)
+    run(board_url, save_path)
